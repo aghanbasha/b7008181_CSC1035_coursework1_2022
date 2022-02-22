44575f5e8f2743d2de853540b87935b694a0a34e
aghanbasha <b7008181@newcastle.ac.uk>
2022-02-22 13:31:43 +0000

    branch functions created

--- src/Branch.java
+++ src/Branch.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Branch {
+
+    private String branch_name;
+    private List
+
+    public Branch(String branch_name) {
+        this.branch_name = branch_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "branch_name='" + branch_name + '\'' +
+                '}';
+    }
+
+    public String getBranch_name() {
+        return branch_name;
+
+
+    }
+
+    public void sale_list (String branch_name, int comparison){
+
+
+
+    }
+
+    public void average_sale(String branch_name){
+        
+    }
 }
