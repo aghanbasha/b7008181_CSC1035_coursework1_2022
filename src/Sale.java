b0a3673a8b4845464e7db56024dde23d9bec5863
aghanbasha <b7008181@newcastle.ac.uk>
2022-02-22 12:39:40 +0000

    Sale class completed

--- src/Sale.java
+++ src/Sale.java
@@ -1,2 +1,39 @@
 public class Sale {
+    private String house_number;
+    private String postcode;
+    private String value;
+    private String date;
+
+    public Sale(String house_number, String postcode, String value, String date) {
+        this.house_number = house_number;
+        this.postcode = postcode;
+        this.value = value;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Sale{" +
+                "house_number='" + house_number + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", value='" + value + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+
+    public String getHouse_number() {
+        return house_number;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
